d038d057a589be037dab6e828ddf43f555fc92be
Anand Singh <reaching.anandsingh@gmail.com>
2020-07-09 10:03:12 +0530

    v1.10

--- global_script.js
+++ global_script.js
@@ -1,9 +1,10 @@
 /* Change Banner Images Automatically */
-var images_banner = ["images/Banner/2.jpeg", 
-					 "images/Banner/3.jpeg", 
-					 "images/Banner/4.jpeg", 
-					 "images/Banner/5.jpeg", 
-					 "images/Banner/6.jpeg"];
+var images_banner = ["images/Banner/2.jpeg",
+  "images/Banner/3.jpeg",
+  "images/Banner/4.jpeg",
+  "images/Banner/5.jpeg",
+  "images/Banner/6.jpeg"
+];
 
 function heroImage() {
   var i = 0;
@@ -23,20 +24,21 @@ function heroImage() {
 
 
 /* Gallery Section */
-var images_gallery = ["images/Gallery/2cc2538f-9db0-4ed4-8cec-03b6e86592c4.JPG", 
-					  "images/Gallery/3bcd04d7-510a-408e-bee7-a675f7adcd18.JPG", 
-					  "images/Gallery/ARP_6797-01-02.jpeg", 
-					  "images/Gallery/ARP_6867-10-01-05-01.jpeg", 
-					  "images/Gallery/ARP_6980-01-01-01.jpeg", 
-					  "images/Gallery/ARP_8440.JPG", 
-					  "images/Gallery/ARP_8522.JPG", 
-					  "images/Gallery/ARP_9511.NEF", 
-					  "images/Gallery/ARP_9617.JPG", 
-					  "images/Gallery/d1bf4def-cbb8-42c7-b48d-26dbae6c7550.JPG", 
-					  "images/Gallery/FB_IMG_1588026530961-01.jpeg", 
-					  "images/Gallery/FB_IMG_1588026600347-01.jpeg", 
-					  "images/Gallery/FB_IMG_1588026909067.jpg", 
-					  "images/Gallery/IMG-2014.JPG"];
+var images_gallery = ["images/Gallery/2cc2538f-9db0-4ed4-8cec-03b6e86592c4.JPG",
+  "images/Gallery/3bcd04d7-510a-408e-bee7-a675f7adcd18.JPG",
+  "images/Gallery/ARP_6797-01-02.jpeg",
+  "images/Gallery/ARP_6867-10-01-05-01.jpeg",
+  "images/Gallery/ARP_6980-01-01-01.jpeg",
+  "images/Gallery/ARP_8440.JPG",
+  "images/Gallery/ARP_8522.JPG",
+  "images/Gallery/ARP_9511.NEF",
+  "images/Gallery/ARP_9617.JPG",
+  "images/Gallery/d1bf4def-cbb8-42c7-b48d-26dbae6c7550.JPG",
+  "images/Gallery/FB_IMG_1588026530961-01.jpeg",
+  "images/Gallery/FB_IMG_1588026600347-01.jpeg",
+  "images/Gallery/FB_IMG_1588026909067.jpg",
+  "images/Gallery/IMG-2014.JPG"
+];
 var currentImageIndex = 0;
 
 function openModal() {
@@ -108,18 +110,18 @@ function nextVideo() {
   document.getElementById("testimonial").src = videos[currentVideoIndex];
 }
 
-function formCheck() {
-  var a = document.getElementById("name").value;
+function resetForm() {
+  /*var a = document.getElementById("name").value;
   var b = document.getElementById("email").value;
   var c = document.getElementById("phone").value;
   var d = document.getElementById("message").value;
   if (a == "" || a == null || b == "" || b == null || c == "" || c == null || d == "" || d == null) {
+	  Event.preventDefault();
     alert("All fields in the form should be filled!");
-  } else {
+  } else {*/
     document.getElementById("contact_form").submit();
     document.getElementById("contact_form").reset();
-    alert("Thank you for reaching out!");
-  }
+  alert("Thank you for reaching out! Someone from our team will get in touch with you shortly.");
 }
 
 function menu_openclose() {
